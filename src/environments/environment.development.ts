b8d56336f0ae55d5a305c8947617c7cbe1195d8a
Manuel Alvarado <manuel.alvarado21@itca.edu.sv>
2025-03-28 14:49:50 -0600

    cambios deploy

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    //apiURL: 'https://alabapi-ad1d5a101b0d.herokuapp.com/api/',
-    apiURL: 'http://localhost:8080/api/',
+    apiURL: 'https://alabapi-ad1d5a101b0d.herokuapp.com/api/',
+    //apiURL: 'http://localhost:8080/api/',
 };
